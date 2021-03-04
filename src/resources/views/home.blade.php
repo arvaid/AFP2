d7ab5829094ca3847f6d65987eeeec57c01c2a5c
Dániel Árvai <arvai96@gmail.com>
2021-03-04 15:38:46 +0100

    Bent maradt debug kód törlése

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -15,7 +15,6 @@
 </head>
 
 <body>
-    {{ asset('img/photo.jpg') }}
     <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
         <div class="container"><a class="navbar-brand logo" href="#">NamesAreOverrated</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
