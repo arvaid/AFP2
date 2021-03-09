09da1ba225212b40e90b7d4412cea35582c0df17
Jámbor Alexandra <ja20000907@gmail.com>
2021-03-09 23:03:09 +0100

    Uuid trait elkészítése
    Uuids.php

---
+++ src/app/Traits/Uuids.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Traits;
+
+
+use Illuminate\Support\Str;
+
+trait Uuids
+{
+    protected static function boot(){
+        parent::boot();
+        static::creating(function($model){
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Str::uuid()->toString();
+            }
+        });
+    }
+    public function getKeyType(){
+        return 'string';
+    }
+    public function getKeyName(){
+        return 'id';
+    }
+}
