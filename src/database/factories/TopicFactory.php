5ab3991ead3205984793a38670cd4b2938ea892a
Jámbor Alexandra <ja20000907@gmail.com>
2021-03-09 22:21:18 +0100

    Topic factory elkészítése

--- src/database/factories/TopicFactory.php
+++ src/database/factories/TopicFactory.php
@@ -7,22 +7,14 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TopicFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
+
     protected $model = Topic::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word(),
         ];
     }
 }
