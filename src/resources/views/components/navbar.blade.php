b223aa5c856f6e31dcbcc8a4a583e1d020a3bc09
Dániel Árvai <arvai96@gmail.com>
2021-03-11 16:20:25 +0100

    Refactor: link helper használata az összes linknél

--- src/resources/views/components/navbar.blade.php
+++ src/resources/views/components/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="container"><a class="navbar-brand logo" href="#">NamesAreOverrated</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link @if(Request::is('/')) active @endif" href="/">Home</a></li>
+                <li class="nav-item"><a class="nav-link @if(Request::is('/')) active @endif" href="{{url('/')}}">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="features.html">Posts</a></li>
                 <li class="nav-item"><a class="nav-link" href="about-us.html">Profile</a></li>
                 <li class="nav-item"><a class="nav-link" href="contact-us.html">support</a></li>
