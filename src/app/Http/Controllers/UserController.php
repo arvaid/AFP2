ce2b4cf9b7c848691a89645cc16e57bdfe3e8645
Dániel Árvai <arvai96@gmail.com>
2021-03-11 15:44:06 +0100

    Regisztrációs route beállítása

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -23,7 +24,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('registration');
     }
 
     /**
@@ -34,7 +35,9 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // TODO: form validáció
+        // TODO: adatbázis művelet
+        // TODO: átirányítás (success-page/error-page)
     }
 
     /**
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [UserController::class, 'create']);
+Route::post('/register', [UserController::class, 'store']);
 
 Route::view('home', 'home')->middleware('auth');
