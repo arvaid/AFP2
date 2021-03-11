d07dee73405d827c59756085ebbce3d7165606e1
Dániel Árvai <arvai96@gmail.com>
2021-03-11 16:08:38 +0100

    Navbar - active csak a főoldalon legyen - főoldal hivatkozás frissítése, hogy működjön is - regisztráció és bejelentkezés linkek hozzáadása

--- src/resources/views/components/navbar.blade.php
+++ src/resources/views/components/navbar.blade.php
@@ -2,12 +2,19 @@
     <div class="container"><a class="navbar-brand logo" href="#">NamesAreOverrated</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+                <li class="nav-item"><a class="nav-link @if(Route::getCurrentRoute()->uri() == '/') active @endif" href="/">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="features.html">Posts</a></li>
                 <li class="nav-item"><a class="nav-link" href="about-us.html">Profile</a></li>
                 <li class="nav-item"><a class="nav-link" href="contact-us.html">support</a></li>
             </ul>
-            <div><i class="fas fa-registered" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 8px;" title="Registration"></i><i class="fas fa-sign-in-alt" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 17px;" title="Login"></i></div>
+            <div>
+                <a href="{{ url('/register') }}">
+                    <i class="fas fa-registered" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 8px;" title="Registration"></i>
+                </a>
+                <a href="{{ url('/login') }}">
+                    <i class="fas fa-sign-in-alt" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 17px;" title="Login"></i>
+                </a>
+            </div>
         </div>
     </div>
 </nav>
