b2759936c0b4972708c3b8972d678ee65d9b9597
Jámbor Alexandra <ja20000907@gmail.com>
2021-03-13 20:31:17 +0100

    User model javítása: HasApiTokens nem volt importálva

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Str;
 use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
