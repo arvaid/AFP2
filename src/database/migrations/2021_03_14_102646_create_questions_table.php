494a1dc6a4dd43ba3b8659e095a211c06f793896
Jámbor Alexandra <ja20000907@gmail.com>
2021-03-14 12:07:00 +0100

    Question migration elkészítése

--- src/database/migrations/2021_03_14_102646_create_questions_table.php
+++ src/database/migrations/2021_03_14_102646_create_questions_table.php
@@ -15,6 +15,7 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->string('question_text');
             $table->timestamps();
         });
     }
