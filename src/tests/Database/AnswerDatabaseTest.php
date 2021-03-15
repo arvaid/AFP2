5c426a7566561ce265522bda161fe7bc8fbaaf6c
Jámbor Alexandra <ja20000907@gmail.com>
2021-03-15 20:11:34 +0100

    Answer adatbázis teszt elkészítve

---
+++ src/tests/Database/AnswerDatabaseTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Database;
+
+use App\Models\Answer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Jenssegers\Mongodb\Relations\BelongsTo;
+use Tests\TestCase;
+
+class AnswerDatabaseTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAnswerDatabaseConnection(): void
+    {
+        try {
+            $result = Answer::all();
+            self::assertNotNull($result);
+        }catch (\Exception $e){
+            self::fail();
+        }
+    }
+
+    public function testAnswerStructure(): void
+    {
+        $subject = Answer::all()->random();
+
+        self::assertNotNull($subject->id);
+        self::assertNotNull($subject->answer_text);
+        self::assertNotNull($subject->question_id);
+        self::assertNotNull($subject->user_id);
+        self::assertNotNull($subject->score);
+        self::assertNotNull($subject->created_at);
+    }
+
+    public function testAnswerModelConnections(): void
+    {
+        $subject = Answer::all()->random();
+
+        self::assertNotNull($subject->question);
+        self::assertNotNull($subject->user);
+
+        self::assertInstanceOf(BelongsTo::class, $subject->question());
+        self::assertInstanceOf(BelongsTo::class, $subject->user());
+    }
+}
