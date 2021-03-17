32b008c17ffbc554c0d36bea6f48ce66678f8f1e
Dániel Árvai <arvai96@gmail.com>
2021-03-17 12:52:08 +0100

    Navbar link formázás alaphelyzetbe állítása

--- src/resources/views/components/navbar.blade.php
+++ src/resources/views/components/navbar.blade.php
@@ -9,15 +9,15 @@
             </ul>
             <div>
                 @guest
-                    <a href="{{ route('register') }}">
+                    <a href="{{ route('register') }}" style="color:inherit;text-decoration: none">
                         <i class="fas fa-user-plus" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 8px;" title="Registration"></i>
                     </a>
-                    <a href="{{ route('login') }}">
+                    <a href="{{ route('login') }}" style="color:inherit;text-decoration: none">
                         <i class="fas fa-sign-in-alt" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 17px;" title="Login"></i>
                     </a>
                 @endguest
                 @auth
-                    <a href="{{ route('logout') }}">
+                    <a href="{{ route('logout') }}" style="color:inherit;text-decoration: none">
                         <i class="fas fa-sign-out-alt" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 8px;" title="Log Out"></i>
                     </a>
                 @endauth
