e7a33d363921f8250392b5519892107bba5a3764
Dániel Árvai <arvai96@gmail.com>
2021-03-21 16:22:21 +0100

    Navbar auth linkek szétválasztása külön fájlba

--- src/resources/views/components/nav/authLinks.blade.php
+++ src/resources/views/components/nav/authLinks.blade.php
@@ -1,29 +1,13 @@
 @guest
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('login') }}">Login</a>
-</li>
-@if (Route::has('register'))
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('register') }}">Register</a>
-</li>
-@endif
-@else
-<li class="nav-item dropdown">
-    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{ Auth::user()->name }}
+    <a href="{{ route('register') }}" style="color:inherit;text-decoration: none">
+        <i class="fas fa-user-plus" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 8px;" title="Registration"></i>
     </a>
-
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <!--suppress JSDeprecatedSymbols : jQuery has a problem with event.preventDefault(). TODO: (low) Fix it if you have time. -->
-        <a class="dropdown-item" href="{{ route('logout') }}"
-           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-            Logout
-        </a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
-    </div>
-</li>
+    <a href="{{ route('login') }}" style="color:inherit;text-decoration: none">
+        <i class="fas fa-sign-in-alt" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 17px;" title="Login"></i>
+    </a>
 @endguest
+@auth
+    <a href="{{ route('logout') }}" style="color:inherit;text-decoration: none">
+        <i class="fas fa-sign-out-alt" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 8px;" title="Log Out"></i>
+    </a>
+@endauth
--- src/resources/views/components/navbar.blade.php
+++ src/resources/views/components/navbar.blade.php
@@ -8,19 +8,7 @@
                 <li class="nav-item"><a class="nav-link" href="contact-us.html">support</a></li>
             </ul>
             <div>
-                @guest
-                    <a href="{{ route('register') }}" style="color:inherit;text-decoration: none">
-                        <i class="fas fa-user-plus" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 8px;" title="Registration"></i>
-                    </a>
-                    <a href="{{ route('login') }}" style="color:inherit;text-decoration: none">
-                        <i class="fas fa-sign-in-alt" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 17px;" title="Login"></i>
-                    </a>
-                @endguest
-                @auth
-                    <a href="{{ route('logout') }}" style="color:inherit;text-decoration: none">
-                        <i class="fas fa-sign-out-alt" data-toggle="tooltip" data-bss-tooltip="" style="padding: 8px;margin: 5px;padding-left: 8px;" title="Log Out"></i>
-                    </a>
-                @endauth
+                @include('components.nav.authLinks')
             </div>
         </div>
     </div>
