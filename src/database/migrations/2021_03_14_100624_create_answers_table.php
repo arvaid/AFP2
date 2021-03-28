017507ca36cc8587178a83601c437483a8d2e4a2
Jámbor Alexandra <ja20000907@gmail.com>
2021-03-28 16:15:29 +0200

    Migration-ök javítása string helyett text típus használatra
    Answers migration, Question migration

--- src/database/migrations/2021_03_14_100624_create_answers_table.php
+++ src/database/migrations/2021_03_14_100624_create_answers_table.php
@@ -15,7 +15,7 @@ class CreateAnswersTable extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->string('answer_text');
+            $table->text('answer_text');
             $table->integer('score')->default(0);
             $table->timestamps();
         });
