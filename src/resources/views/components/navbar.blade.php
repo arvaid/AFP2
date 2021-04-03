1a2a45eca323d1a42f1d9128b6f025a26850bb37
Jámbor Alexandra <ja20000907@gmail.com>
2021-04-03 16:08:53 +0200

    Nav-bar logójának linkelhetővé tétele

--- src/resources/views/components/navbar.blade.php
+++ src/resources/views/components/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
-    <div class="container"><a class="navbar-brand logo" href="#">NamesAreOverrated</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <div class="container"><a class="navbar-brand logo" href="{{ route('home') }}">NamesAreOverrated</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link @if(Request::is('/')) active @endif" href="{{url('/')}}">Home</a></li>
+                <li class="nav-item"><a class="nav-link @if(Request::is('/')) active @endif" href="{{ route('home') }}">Home</a></li>
                 <li class="nav-item"><a class="nav-link @if(Route::is('question.*'))) active @endif" href="{{ route('question.index') }}">Posts</a></li>
                 <li class="nav-item"><a class="nav-link" href="about-us.html">Profile</a></li>
                 <li class="nav-item"><a class="nav-link" href="contact-us.html">support</a></li>
