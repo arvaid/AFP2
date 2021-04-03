13b6a57803d07eeaf854a647df55cc6c7ba381ef
Jámbor Alexandra <ja20000907@gmail.com>
2021-04-03 14:44:22 +0200

    Felesleges paraméter eltávolítása a question-handler.js-ből

--- src/public/js/question-handler.js
+++ src/public/js/question-handler.js
@@ -9,13 +9,13 @@ function loadTopics(topicIndex){
     xmlHttpRequest.send()
 }
 
-function loadQuestionsFor(id, topicShow){
+function loadQuestionsFor(topicShow){
     let xmlHttpRequest = new XMLHttpRequest()
     xmlHttpRequest.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             document.getElementById("questionList").innerHTML = this.responseText
         }
     };
-    xmlHttpRequest.open("GET", `${topicShow}/${id}`, true)
+    xmlHttpRequest.open("GET", `${topicShow}`, true)
     xmlHttpRequest.send()
 }
