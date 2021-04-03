abcf9e87fa9ab12e1351cfa122b0ecaa59a9e1f1
Jámbor Alexandra <ja20000907@gmail.com>
2021-04-03 15:24:19 +0200

    CSRF token eltávolítása a layout-ról

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+{{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}} <!-- Ha nem muszáj ne legyen itt -->
 
     @hasSection('title')
         <title>@yield('title')</title>
