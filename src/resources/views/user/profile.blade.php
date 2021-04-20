7762a3383be504f8b7afb112d8f54abd21f58e69
Jámbor Alexandra <ja20000907@gmail.com>
2021-04-20 20:18:48 +0200

    Profile oldal formázása
    Táblázatba foglalás, margók kialakítása

--- src/resources/views/user/profile.blade.php
+++ src/resources/views/user/profile.blade.php
@@ -9,19 +9,39 @@
         /** @var User $user */
     ?>
     <section class="page container-fluid">
-        <div class="d-block" id="profile-static">
-            Username: {{ $user->username }}
-            Email: {{ $user->email }}
-            Score: {{ $user->score }}
-            Rank: {{ $user->rank }}
-            Questions count: {{ $user->questions()->count() }}
-            Answer count: {{ $user->answers()->count() }}
+        <div class="d-block m-1 mt-4 ml-md-4" id="profile-static">
+            <table>
+                <tr>
+                    <td><b>Username:</b></td>
+                    <td class="px-2">{{ $user->username }}</td>
+                </tr>
+                <tr>
+                    <td><b>Email:</b></td>
+                    <td class="px-2">{{ $user->email }}</td>
+                </tr>
+                <tr>
+                    <td><b>Score:</b></td>
+                    <td class="px-2">{{ $user->score }}</td>
+                </tr>
+                <tr>
+                    <td><b>Rank:</b></td>
+                    <td class="px-2">{{ $user->rank }}</td>
+                </tr>
+                <tr>
+                    <td><b>Questions count:</b></td>
+                    <td class="px-2">{{ $user->questions()->count() }}</td>
+                </tr>
+                <tr>
+                    <td><b>Answer count:</b></td>
+                    <td class="px-2">{{ $user->answers()->count() }}</td>
+                </tr>
+            </table>
         </div>
         <div>
-            <button class="btn btn-warning" id="edit_profile_btn" onclick="editProfile()">Edit Profile</button>
-            <button class="btn btn-danger d-none" id="cancel_edit_profile_btn" onclick="cancelEditProfile()">Cancel Edit</button>
+            <button class="btn btn-warning m-1 mt-1 ml-md-4" id="edit_profile_btn" onclick="editProfile()">Edit Profile</button>
+            <button class="btn btn-danger d-none ml-md-2 mt-3" id="cancel_edit_profile_btn" onclick="cancelEditProfile()">Cancel Edit</button>
         </div>
-        <div class="d-none" id="profile-edit">
+        <div class="d-none ml-md-2 mt-2" id="profile-edit">
             <form method="post" action="{{ route('user.update', [$user]) }}">
                 @method('put')
                 @csrf
@@ -39,7 +59,7 @@
                 <div class="form-group">
                     <label for="new_password">New Password:</label>
                     <input type="password" class="form-control" name="new_password" id="new_password" />
-                    <label for="new_password_confirmation">New Password Confirmation:</label>
+                    <label class="mt-1" for="new_password_confirmation">New Password Confirmation:</label>
                     <input type="password" class="form-control" name="new_password_confirmation" id="new_password_confirmation" />
                 </div>
 
